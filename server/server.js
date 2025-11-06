2db90e167bd023a58e24e2e1d8913a6c1aba6054
bece21-mkenani <mphatsokenani0@gmail.com>
2025-11-06 12:04:12 +0000

    working version of chart app

--- server/server.js
+++ server/server.js
@@ -10,10 +10,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
-app.use(express.static(path.join(__dirname, '../client/dist')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-});
 
 // Create uploads directory if it doesn't exist
 if (!fs.existsSync('uploads')) {
