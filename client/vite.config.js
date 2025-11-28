72756208c736ff1aa75be88bcf904fc9de6dc085
bece21-mkenani <mphatsokenani0@gmail.com>
2025-11-28 23:10:28 +0200

    cleaned the repo

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,19 +6,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react(),
      tailwindcss()],
-  // server: {
-  //   port: 5173,
-  //   proxy: {
-  //     '/socket.io': {
-  //       target: 'http://localhost:5000',
-  //       changeOrigin: true,
-  //       ws: true
-  //     }
-  //   }
-  // },
-  // build: {
-  //   outDir: 'dist',
-  //   sourcemap: false,
-  //   minify: 'esbuild'
-  // }
+  server: {
+    port: 5173,
+    proxy: {
+      '/socket.io': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        ws: true
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild'
+  }
 })
